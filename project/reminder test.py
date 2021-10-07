7d354139fb0bed810c254cdbbcb1e9fde6a0dd74
skyrimwell <51721698+skyrimwell@users.noreply.github.com>
2021-10-07 17:48:01 +0300

    Добавлена функция поиска людей по месяцу, добавлены дополннительные тесты

--- project/reminder test.py
+++ project/reminder test.py
@@ -5,8 +5,16 @@ from reminder import BirthdayBook
 class ReminderTest(unittest.TestCase):
     def setUp(self):
         person1 = Person("Саня", datetime(2001, 9, 9))
+        person2 = Person("Леха", datetime(1999, 2, 6))
+        person3 = Person("Серега", datetime(2001, 9, 10))
+        person4 = Person("Вика", datetime(2001, 11, 28))
+        person5 = Person("Еще один серега", datetime(2000, 9, 10))
         self.birthdayBook = BirthdayBook()
         self.birthdayBook.addPerson(person1)
+        self.birthdayBook.addPerson(person2)
+        self.birthdayBook.addPerson(person3)
+        self.birthdayBook.addPerson(person4)
+        self.birthdayBook.addPerson(person5)
 
     def testDay(self):
         self.assertEqual(self.birthdayBook.printBirthDateByName("Саня"), ['Саня', 9, 9, 2001])
@@ -14,7 +22,10 @@ class ReminderTest(unittest.TestCase):
         self.assertEqual(self.birthdayBook.printBirthDateByName("ААААА"), None)
     def testDay2(self):
         self.assertEqual(self.birthdayBook.printBirthDateByName("Саняяяя"), None)
-
+    def testDay3(self):
+        self.assertEqual(self.birthdayBook.printBirthdayByMonth(9), ['Саня', 'Серега', 'Еще один серега'])
+    def testDay4(self):
+        self.assertEqual(self.birthdayBook.printBirthdayByMonth(1), None)
 
 if __name__ == '__main__':
     unittest.main()
--- project/reminder.py
+++ project/reminder.py
@@ -12,6 +12,7 @@ class Person:
         return self.name
     def setBirthDate(self, birthdate):
         self.birthdate = birthdate
+
     def getBirthDate(self):
         return self.birthdate
 
@@ -25,17 +26,33 @@ class BirthdayBook:
         if self.personList.get(name):
             return self.personList.get(name).getBirthDate()
         return None
+    def _getPersonByBirthDateMonth(self, month):
+        marches = [];
+        for p in self.personList.values():
+            if p.getBirthDate().month == month:
+                marches.append(p)
+        return marches
     def printBirthDateByName(self, name):
         d = self._getBirthdayByName(name)
         if d!= None:
             print("Имя: ", name, " День рождения: ", d.day, "/", d.month, "/", d.year)
             return [name, d.day, d.month, d.year]
 
+    def printBirthdayByMonth(self, month):
+        marches = self._getPersonByBirthDateMonth(month)
+        if marches != []:
+            print("Родились в месяца: ", month)
+            for p in marches:
+                print(p.getName())
+            return [p.getName() for p in marches]
+        else:
+            return None
+
 
 if __name__ == '__main__':
     person1 = Person("Саня", datetime(2001,9,9))
     birthdayBook=BirthdayBook()
     birthdayBook.addPerson(person1)
-
+    birthdayBook.printBirthdayByMonth(9)
     print(birthdayBook.printBirthDateByName("Саня"))
 
